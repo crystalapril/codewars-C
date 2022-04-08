fb73cf2e7c650d5070d84e5cd132b8ba3438feb4
crystalapril <aprilzhangpku@qq.com>
2022-04-08 17:01:35 +0800

    Create function-3-multiplying-two-numbers.c

---
+++ function-3-multiplying-two-numbers.c
@@ -0,0 +1,17 @@
+'''
+tag:
+multiple 
+'''
+
+#answer1.0 by april
+int multiply(int a, int b) {
+    return a*b;  
+}
+
+#answer2.0
+long multiply(int x, int y) {
+  return x * y;
+}
+
+#answer3.0
+multiply(x,y){return x*y;}
