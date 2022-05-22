168c774469e71c013f31bb7186e305cee583df7e
crystalapril <aprilzhangpku@qq.com>
2022-05-22 22:57:43 +0800

    Update convert-a-number-to-a-string.c

--- convert-a-number-to-a-string.c
+++ convert-a-number-to-a-string.c
@@ -3,7 +3,7 @@ tag:
 else if, pointer, struct, function
 '''
   
-#answer1.0 by april
+#answer1.0 by april : pointer
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -28,7 +28,6 @@ char *number_to_string(int number) {
   while (temp > 9 ) {
     temp /= 10;
     count++;
-//    printf("temp= %d,count= %d\n",temp,count);
     fac *= 10;
   }
   char *p = calloc(count+3,sizeof(char));
@@ -37,11 +36,9 @@ char *number_to_string(int number) {
 
   for (; i< count + e; i++){
     int r = number / fac;
-//    printf("fac=%d ,r=%d\n",fac,r);
     p[i] = r+ '0';
     number = number % fac;
     fac /= 10;
-//    printf("i = %d,p[i] = %c, number = %d\n",i,p[i],number);
   }
   p[count + e] = '\0';
   printf("p = %s\n",p);
@@ -69,5 +66,48 @@ int main(void) {
 }
 
 
-#answer2.0 
+#answer2.0 by april : struct
+struct sign {
+    int i;
+    int e;
+    int n;
+};
+
+struct sign ifPositive(int number,char p[]) {
+    struct sign s;
+    if (number < 0) {
+        s.i = 1;
+        s.e = 2;
+        s.n = - number;
+        p[0] = '-';
+        return s;
+    } else if (number >= 0) {
+        s.i = 0;
+        s.e = 1;
+        s.n = number;
+        return s;
+    }
+}
+
+char *number_to_string(int number) {
+  int count = 0;
+  int temp = abs(number);
+  int fac = 1;
+  while (temp > 9 ) {
+    temp /= 10;
+    count++;
+    fac *= 10;
+  }
+  char *p = calloc(count+3,sizeof(char));
+  struct sign s = ifPositive(number,p);
+  for (; s.i< count + s.e; s.i++){
+    int r = s.n / fac;
+    p[s.i] = r + '0';
+    s.n = s.n % fac;
+    fac /= 10;
+  }
+  p[count + s.e] = '\0';
+
+  return p;
+}
 
