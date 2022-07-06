0ba5f5b5310167b47ed4c59fbadd9068b3dea996
crystalapril <aprilzhangpku@qq.com>
2022-07-06 15:57:58 +0800

    Create is-this-my-tail.c

---
+++ is-this-my-tail.c
@@ -0,0 +1,45 @@
+/*
+tag:
+bool, array, strcmp, 
+*/
+
+#answer1.0 by april
+int correct_tail(const char *body, const char *tail) {
+  int len = strlen(body);
+  char last = body[len-1];
+  if (last == *tail )  
+    return 1;
+  else 
+    return 0;
+}
+
+
+#answer2.0
+int correct_tail(const char *body, const char tail[1]) {
+  return body[strlen(body) - 1] == *tail;
+}
+
+
+#answer3.0
+int correct_tail(const char *body, const char *tail) {
+  
+  int i = 0;
+  while (body[i]!='\0') {
+    i++;
+  }
+  return body[i-1] == *tail ? 1 : 0;
+}
+
+
+#answer4.0
+#include <string.h>
+
+int correct_tail(const char *body, const char *tail) {
+
+if (strcmp(tail, &body[strlen(body) - 1]) == 0)
+    return 1;
+else
+  return 0;
+}
+//  int strcmp(const char *str1, const char *str2) 把 str1 所指向的字符串和 str2 所指向的字符串进行比较
+
