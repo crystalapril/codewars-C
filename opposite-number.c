01bebd84336a4c3d52a070c5997890a498e036ba
crystalapril <aprilzhangpku@qq.com>
2022-07-07 16:05:25 +0800

    Create opposite-number.c

---
+++ opposite-number.c
@@ -0,0 +1,20 @@
+/*
+tag:
+num
+*/
+
+#answer1.0 by april
+float opposite(float num) {
+  return 0.0-num;
+}
+
+
+#answer2.0
+float opposite(float num) {
+  return -num;
+}
+
+#answer3.0
+float opposite(float num) {
+  return num * -1;
+}
