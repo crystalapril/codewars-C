5401fcef271b1bdf7dc5593c5424ae973c74bcae
crystalapril <aprilzhangpku@qq.com>
2022-07-07 14:06:15 +0800

    Create volume-of-a-cuboid.c

---
+++ volume-of-a-cuboid.c
@@ -0,0 +1,17 @@
+/*
+tag:
+multiply
+*/
+
+#answer1.0 by april
+double getVolumeOfCuboid(double length, double width, double height) {
+  double c= length * width * height;
+  return c;
+}
+
+
+#answer2.0 
+double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width *height;
+}
+
